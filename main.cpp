c5bb6cd5d0b57d77dac3c1b72430b6e7a8be658f
haelbouhad <hassan.bouhadi@gmail.com>
2016-12-21 17:16:27 +0100

    remove commented code

--- main.cpp
+++ main.cpp
@@ -14,28 +14,4 @@ int main(int argc, char** argv){
 
     f.compute();
     
-    /*
-    CImg<unsigned char> image(filename.c_str());
-    CImgDisplay input(image, "Image de depart");
-
-    CImg<float> im_gauss = ApplyGaussian(image,4);
-    CImgDisplay gauss(im_gauss, "Image apres le filtre gaussien");
-
-    CImg<unsigned char> im_canny = CannyEdges(im_gauss, 2.5, 7.5, true);
-    CImgDisplay canny(im_canny, "Image apres Canny"); 
-    //im_canny.normalize(0, 255);
-    //im_canny.save("img/out_canny.bmp");
-
-    CImg<unsigned char> im_hough = hough_cercles(im_canny, 60);
-    CImgDisplay hough_fct(im_hough, "Image resultante de Hough");
-
-    im_hough = hough_creer_cercles(im_hough, 600, 60);
-    CImgDisplay hough_cercles(im_hough, "Image de cercles");
-
-
-
-    while(!canny.is_closed()){
-        canny.wait();
-    }
-*/
 }
